f0e2d41a37d114bf0119e56b6bf46954a6becd21
Geetha-Sekarr <geetha.sekar@aspiresys.com>
2024-12-27 19:44:22 +0530

    27-12-2024

---
+++ dynamic.js
@@ -0,0 +1,8 @@
+let firstname="geetha";
+console.log(typeof firstname);
+let age=12;
+console.log(typeof age)
+let lastname="sekar";
+console.log(typeof lastname)
+let nickname=undefined;
+console.log(typeof nickname)
